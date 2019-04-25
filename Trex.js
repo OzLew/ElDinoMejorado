4666dbc92fb02336e2a925bc659b2a34b9887ef4
OzLew <32676515+OzLew@users.noreply.github.com>
2019-04-25 10:05:57 -0600

    Add files via upload

---
+++ Trex.js
@@ -0,0 +1,50 @@
+class Trex{
+	constructor(animation, x, y){
+		this.animation = animation 
+		this.x = x
+		this.y = y
+		this.index = 0
+		this.salto = false
+	}
+
+	draw(){
+		image(this.animation[floor(this.index) % 6], this.x, this.y)
+	}
+
+	move(){
+		if(!this.salto)
+			this.index++;
+		else {
+			if(this.y < 152) {
+				this.y += 5.5
+			} else {
+				this.salto = false
+				this.index++;
+			}
+		}
+
+	}
+
+	getY(){
+		return this.y;
+	}
+
+	bajar(){
+		this.y++
+		//this.salto=false
+	}
+	
+	jump(){
+		if(!this.salto){
+			for(var i = 0; i < 22; i += 2){
+				this.y-=i;
+			}
+			this.salto=true
+		}/*else if(this.y<=152){
+			this.y++
+		}
+		if(this.y==152){
+			this.salto=false
+		}*/
+	}
+}
